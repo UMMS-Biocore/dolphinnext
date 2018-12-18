6b10b02f1e87ca082ed03fa454995d902c739efc
Onur Yukselen <onuryukselen@gmail.com>
2018-12-18 14:20:44 -0500

    travis fix

--- db/updateVersion0.2.54.sql
+++ db/updateVersion0.2.54.sql
@@ -5,4 +5,4 @@ ALTER TABLE `profile_amazon`
 ADD `public` int(2) DEFAULT NULL AFTER `id`;
 
 ALTER TABLE `profile_cluster`
-MODIFY job_queue VARCHAR(25) DEFAULT NULL AFTER `job_time`,
+MODIFY job_queue VARCHAR(25) DEFAULT NULL AFTER `job_time`;
