354e60370c5e2ab456fe336a4c744c31188efe7d
onuryukselen <onuryukselen@gmail.com>
2022-06-08 13:27:29 -0400

    03.06.2021 Version 1.3.14

--- public/js/googlesign.js
+++ public/js/googlesign.js
@@ -1,37 +1,43 @@
 // google sign-in
-function Google_signIn(googleUser) {
-    var id_token = googleUser.getAuthResponse().id_token;
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.disconnect();
-
-    var userProfile = [];
-    var profile = googleUser.getBasicProfile();
-    var emailUser = profile.getEmail();
-    var pattEmail = /(.*)@(.*)/;
-    userProfile.push({ name: "google_id", value: profile.getId() });
-    userProfile.push({name: "name",value: profile.getName()});
-    userProfile.push({name: "email",value: profile.getEmail()});
-    userProfile.push({name: "google_image",value: profile.getImageUrl()});
-    userProfile.push({name: "p",value: 'saveGoogleUser'});
+function handleCredentialResponse(response) {
+    let responsePayload = jwt_decode(response.credential)
+    let userProfile = [];
+    userProfile.push({ name: "google_id", value: responsePayload.sub });
+    userProfile.push({ name: "name", value: responsePayload.name });
+    userProfile.push({ name: "email", value: responsePayload.email });
+    userProfile.push({ name: "google_image", value: responsePayload.picture });
+    userProfile.push({ name: "p", value: 'saveGoogleUser' });
     $.ajax({
         type: "POST",
         data: userProfile,
         url: "ajax/ajaxquery.php",
-        async: false,
-        success: function (msg) {
+        async: true,
+        success: function(msg) {
             if (msg.error == 1) {
                 alert('Something Went Wrong!');
             } else {
                 var logInSuccess = true;
-                console.log($('#basepathinfo').attr('sso_login'))
-                if ($('#basepathinfo').attr('sso_login') === "1"){
-                     window.location.replace("php/after-sso.php");
+                if ($('#basepathinfo').attr('sso_login') === "1") {
+                    window.location.replace("php/after-sso.php");
                 } else {
-                     window.location.replace("index.php");
+                    window.location.replace("index.php");
                 }
-               
+
             }
         }
     });
-}
 
+}
+window.onload = function() {
+    const google_client_id = document.getElementById("basepathinfo").getAttribute("google_client_id");
+    if (google_client_id) {
+        google.accounts.id.initialize({
+            client_id: google_client_id,
+            callback: handleCredentialResponse
+        });
+        google.accounts.id.renderButton(
+            document.getElementById("googleSignIn"), { theme: "outline", size: "large", "width": "199" }
+        );
+    }
+
+}
--- public/php/newuserform.php
+++ public/php/newuserform.php
@@ -4,9 +4,9 @@ $first_name = "";
 $last_name = "";
 $name = isset($_SESSION['name']) ? $_SESSION['name'] : "";
 $google_login = isset($_SESSION['google_login']) ? $_SESSION['google_login'] : "";
-if (!isset($_SESSION['username']) && $google_login != ""){
-    $google_singup = true; 
-    if (strpos($name, " ") !== false){
+if (!isset($_SESSION['username']) && $google_login != "") {
+    $google_singup = true;
+    if (strpos($name, " ") !== false) {
         $name = trim($name);
         $parts = explode(" ", $name);
         $last_name = array_pop($parts);
@@ -22,7 +22,7 @@ if (!isset($_SESSION['username']) && $google_login != ""){
 <head class="bg-gray">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?php echo COMPANY_NAME?> DolphinNext Pipeline Builder</title>
+    <title><?php echo COMPANY_NAME ?> DolphinNext Pipeline Builder</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!--   app’s client ID prodcued in the Google Developers Console-->
@@ -56,7 +56,6 @@ if (!isset($_SESSION['username']) && $google_login != ""){
         .footer {
             border: 1px solid #B7BFC6 !important
         }
-
     </style>
 </head>
 
@@ -69,31 +68,40 @@ if (!isset($_SESSION['username']) && $google_login != ""){
           <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
         <![endif]-->
 
-<body  style="background:linear-gradient(to top right, #7474BF 10%, #5c7cc2 55%, #348AC7 100%);">
+<body style="background:linear-gradient(to top right, #7474BF 10%, #5c7cc2 55%, #348AC7 100%);">
     <div name="empty_space" style="height:10%; width:100%; clear:both;"></div>
     <div class="form-box" id="login-box" style="width:470px;">
-        <h1 class="text-center" style="padding-bottom:20px;"><span class="logo-lg" style="color:white; font-size:30px;"><b><?php echo COMPANY_NAME?></b> DolphinNext</span></h1>
-        <form action="<?php echo BASE_PATH?>/" method="post">
+        <h1 class="text-center" style="padding-bottom:20px;"><span class="logo-lg" style="color:white; font-size:30px;"><b><?php echo COMPANY_NAME ?></b> DolphinNext</span></h1>
+        <form action="<?php echo BASE_PATH ?>/" method="post">
             <div class="body bg-white" style=" border-radius:5px; padding:30px;">
                 <div style="margin:auto; height:80px; padding-top:20px;">
                     <h2 class="text-center">
-                    <?php
-                if ($google_singup == true){echo "Sign Up with Google";}
-                else {echo "Sign Up";}
-                    ?></h2>
+                        <?php
+                        if ($google_singup == true) {
+                            echo "Sign Up with Google";
+                        } else {
+                            echo "Sign Up";
+                        }
+                        ?></h2>
                 </div>
                 <div class="text-center form-group">
-                    <input type="text" name="firstname" class="form-control" placeholder="First name" maxlength="25" value="<?php 
-                        if (isset($firstname_val)) {echo $firstname_val; } 
-                        else if ($first_name != "") {echo $first_name;}
-                        ?>" />
-                    <?php  if (isset($err_firstname)) echo $err_firstname; ?>
+                    <input type="text" name="firstname" class="form-control" placeholder="First name" maxlength="25" value="<?php
+                                                                                                                            if (isset($firstname_val)) {
+                                                                                                                                echo $firstname_val;
+                                                                                                                            } else if ($first_name != "") {
+                                                                                                                                echo $first_name;
+                                                                                                                            }
+                                                                                                                            ?>" />
+                    <?php if (isset($err_firstname)) echo $err_firstname; ?>
                 </div>
                 <div class="text-center form-group">
-                    <input type="text" name="lastname" class="form-control" placeholder="Last name" maxlength="20" value="<?php 
-                        if (isset($lastname_val)) {echo $lastname_val;}
-                        else if ($last_name != "") {echo $last_name;} 
-                    ?>" />
+                    <input type="text" name="lastname" class="form-control" placeholder="Last name" maxlength="20" value="<?php
+                                                                                                                            if (isset($lastname_val)) {
+                                                                                                                                echo $lastname_val;
+                                                                                                                            } else if ($last_name != "") {
+                                                                                                                                echo $last_name;
+                                                                                                                            }
+                                                                                                                            ?>" />
                     <?php if (isset($err_lastname)) echo $err_lastname;  ?>
                 </div>
                 <div class="text-center form-group">
@@ -101,11 +109,11 @@ if (!isset($_SESSION['username']) && $google_login != ""){
                     <?php if (isset($err_username)) echo $err_username;  ?>
                 </div>
                 <?php
-                if ($google_singup == false){
+                if ($google_singup == false) {
                     echo '<div class="text-center form-group"><input type="text" name="email" class="form-control" maxlength="45" placeholder="Email" value="';
                     if (isset($email_val)) echo $email_val;
                     echo '" />';
-                    if (isset($err_email)) echo $err_email; 
+                    if (isset($err_email)) echo $err_email;
                     echo '</div>';
                 }
                 ?>
@@ -118,11 +126,11 @@ if (!isset($_SESSION['username']) && $google_login != ""){
                     <?php if (isset($err_lab)) echo $err_lab ?>
                 </div>
                 <?php
-                if ($google_singup == false){
+                if ($google_singup == false) {
                     echo '<div class="text-center form-group"><input type="password" name="password" class="form-control password" placeholder="Password" value="';
                     if (isset($password_val)) echo $password_val;
                     echo '" />';
-                    if (isset($err_password)) echo $err_password; 
+                    if (isset($err_password)) echo $err_password;
                     echo '</div>';
                     echo '<div class="text-center form-group"><input type="password" name="verifypassword" class="form-control password" placeholder="Verify Password" value=""/>';
                     if (isset($err_verifypassword)) echo $err_verifypassword;
@@ -139,8 +147,8 @@ if (!isset($_SESSION['username']) && $google_login != ""){
 
 
     </div>
-    <!--Google Platform Library on your web pages that integrate Google Sign-In-->
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <!--Google client library for Google Sign-In-->
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js" type="text/javascript"></script>
 
